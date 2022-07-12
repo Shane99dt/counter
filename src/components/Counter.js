201aaa3441bdd5623f6635b24130bc2e42047e3c
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-12 15:29:20 +0200

    simple bootstrap

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,16 +5,11 @@ class Counter extends React.Component {
     return (
       <div>
         <h1>Counter</h1>
-        <div className='d-flex flex-row col-4 align-items-center'>
-          <button onClick={this.props.substract} className='btn btn-success m-2' >-</button>
+        <div className='d-flex flex-row col-4 align-items-center border justify-content-center p-4'>
+          <button onClick={this.props.substract} className='btn btn-success m-2 col-4' >-</button>
           <h2 className='mx-4' >{this.props.count}</h2>
-          <button className='btn btn-danger m-2' onClick={this.props.increment}>+</button>
+          <button className='btn btn-danger m-2 col-4' onClick={this.props.increment}>+</button>
         </div>
-        {/* <div className='d-flex flex-row col-4 align-items-center'>
-          <button onClick={this.handleClickDown} className='btn btn-success m-2' >-</button>
-          <h2 className='mx-4' >{this.state.count}</h2>
-          <button className='btn btn-danger m-2' onClick={this.handleClickUp}>+</button>
-        </div> */}
       </div>
     )
   }
